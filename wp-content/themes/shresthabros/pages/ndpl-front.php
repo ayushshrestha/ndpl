626ba8680329a24c0482e459f2e50779d2cb0155
Ayush Shrestha <ayushfrontend@gmail.com>
2024-02-26 11:59:47 +0545

    home page domestic form ui form changes

--- wp-content/themes/shresthabros/pages/ndpl-front.php
+++ wp-content/themes/shresthabros/pages/ndpl-front.php
@@ -368,7 +368,7 @@
             
         </div>
         <div id="modelConfirm" class="fixed hidden z-50 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 transition duration-700 ease">
-            <div class="relative top-40 mx-auto shadow-xl rounded-md bg-white max-w-xl">
+            <div class="relative top-2 mx-auto shadow-xl rounded-md bg-white max-w-xl">
 
                 <div class="flex justify-end p-2">
                     <button onclick="closeModal('modelConfirm')" type="button"
@@ -381,22 +381,9 @@
                     </button>
                 </div>
 
-                <div class="p-6 pt-0 text-center">
-                    <div class="border border-b-2 border-gray-200">
-                        <h3 class="text-xl font-normal text-gray-500 mt-5 mb-6">Domestic Dealership</h3>
-                        <div class="">
-                        <?php echo do_shortcode('[contact-form-7 id="8075b32" title="Domestic Dealership"]') ;?>
-                        </div>
-                    </div>
-                    <a href="#"  onclick="closeModal('modelConfirm')"
-                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2">
-                        Yes, I'm sure
-                    </a>
-                    <a href="#" onclick="closeModal('modelConfirm')"
-                        class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center"
-                        data-modal-toggle="delete-user-modal">
-                        No, cancel
-                    </a>
+                <div class="p-6 pt-0">
+                    <h3 class="text-xl font-bold text-gray-500 mt-5 mb-6">Domestic Dealership</h3>
+                    <?php echo do_shortcode('[contact-form-7 id="8075b32" title="Domestic Dealership"]') ;?>
                 </div>
 
             </div>
